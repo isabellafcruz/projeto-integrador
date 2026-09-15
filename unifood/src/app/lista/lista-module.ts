2dc43d845d6c0f4e004f240b1a37b8e2885dadb4
isabellafcruz <128875509+isabellafcruz@users.noreply.github.com>
2026-09-15 01:35:59 +0000

    exerc04

--- unifood/src/app/lista/lista-module.ts
+++ unifood/src/app/lista/lista-module.ts
@@ -8,14 +8,10 @@ import { ListaRoutingModule } from './lista-routing-module';
 import { Exerc01 } from './exerc01/exerc01';
 import { Exerc02 } from './exerc02/exerc02';
 import { Exerc03 } from './exerc03/exerc03';
+import { Exerc04 } from './exerc04/exerc04';
 
 @NgModule({
-  declarations: [Exerc01, Exerc02, Exerc03],
-  imports: [
-    CommonModule,
-    FormsModule,
-    CurrencyPipe,
-    ListaRoutingModule
-  ],
+  declarations: [Exerc01, Exerc02, Exerc03, Exerc04],
+  imports: [CommonModule, FormsModule, CurrencyPipe, ListaRoutingModule],
 })
 export class ListaModule {}
--- unifood/src/app/lista/lista-routing-module.ts
+++ unifood/src/app/lista/lista-routing-module.ts
@@ -5,11 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { Exerc01 } from './exerc01/exerc01';
 import { Exerc02 } from './exerc02/exerc02';
 import { Exerc03 } from './exerc03/exerc03';
+import { Exerc04 } from './exerc04/exerc04';
 
 const routes: Routes = [
   { path: 'exerc01', component: Exerc01 },
   { path: 'exerc02', component: Exerc02 },
-  { path: 'exerc03', component: Exerc03 }
+  { path: 'exerc03', component: Exerc03 },
+  {path: 'exerc04',  component: Exerc04 }
 ];
 
 @NgModule({
